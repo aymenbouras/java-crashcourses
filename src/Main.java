146aeed9eeb1899cf9607680ce23ec7c30a31d4f
aymenbouras <aymenbouras99@gmail.com>
2020-10-14 13:41:37 +0200

    java crash course Day2

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,22 @@
 //Package à importer afin d'utiliser l'objet File
 import java.io.File;
+import java.lang.ReflectiveOperationException;
+import java.lang.reflect.Constructor;
 
 public class Main {
     public static void main(String[] args) {
-        //Création de l'objet File
-        File f = new File("wesh.txt");
-        System.out.println("Chemin absolu du fichier : " + f.getAbsolutePath());
-        System.out.println("Nom du fichier : " + f.getName());
-        System.out.println("Est-ce qu'il existe ? " + f.exists());
-        System.out.println("Est-ce un répertoire ? " + f.isDirectory());
-        System.out.println("Est-ce un fichier ? " + f.isFile());
+        Class c = new String().getClass();
+        Constructor[] construc = c.getConstructors();
+        System.out.println("Il y a " + construc.length + " constructeurs dans cette classe");
+        //On parcourt le tableau des constructeurs
+        for(int i = 0; i < construc.length; i++){
+            System.out.println(construc[i].getName());
 
-        System.out.println("Affichage des lecteurs à la racine du PC : ");
-        for(File file : f.listRoots())
-        {
-            System.out.println(file.getAbsolutePath());
-            try {
-                int i = 1;
-                //On parcourt la liste des fichiers et répertoires
-                for(File nom : file.listFiles()){
-                    //S'il s'agit d'un dossier, on ajoute un "/"
-                    System.out.print("\t\t" + ((nom.isDirectory()) ? nom.getName()+"/" : nom.getName()));
+            Class[] param = construc[i].getParameterTypes();
+            for(int j = 0; j < param.length; j++)
+                System.out.println(param[j]);
 
-                    if((i%4) == 0){
-                        System.out.print("\n");
-                    }
-                    i++;
-                }
-                System.out.println("\n");
-            } catch (NullPointerException e) {
-                //L'instruction peut générer une NullPointerException
-                //s'il n'y a pas de sous-fichier !
-            }
+            System.out.println("-----------------------------\n");
         }
     }
 }
